57f4361dd4f7d4a6fc40a3958a2f3236128fa828
pelkoa-glitch <hvngmvnvibe@gmail.com>
2024-05-16 16:32:43 +0300

    Added Product Services and API with filters

---
+++ core/api/filters.py
@@ -0,0 +1,14 @@
+from ninja import Schema
+
+
+class PaginationIn(Schema):
+    offset: int = 0
+    limit: int = 20
+
+
+class PaginationOut(Schema):
+    offset: int
+    limit: int
+    total: int
+
+    
--- core/apps/products/admin.py
+++ core/apps/products/admin.py
@@ -5,5 +5,5 @@ from core.apps.products.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', )
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_visible',)
     
